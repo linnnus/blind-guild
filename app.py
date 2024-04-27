linnnus/blind-guild

commit 2bf130581b763819672551c138cc70119005ef93

Add form validation on join page

app.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, run, debug, static_file, request, redirect, response
+from bottle import Bottle, run, debug, static_file, request, redirect, response, HTTPError
 from bottle import jinja2_template as template
 from oauthlib.oauth2 import WebApplicationClient
 from requests_oauthlib import OAuth2Session
@@ -46,6 +46,23 @@ def callback():
 @app.route("/join.html")
 def join_form():
     return template("join")
+
+@app.route("/join.html", method="POST")
+def join_submission(db):
+    name = request.forms.get("name")
+    preferred_role = request.forms.get("preferredRole")
+    motivation = request.forms.get("motivation")
+
+    if name == None or name.strip() == "":
+        raise HTTPError(400, "Namefield is empty or missing. ( warning: this is not good )")
+    if preferred_role == None:
+        raise HTTPError(400, "Preferred role is empty or missing.")
+    if preferred_role not in ("dps", "tank", "healer"):
+        raise HTTPError(400, "Preferred role must be one of the options (DPS, Tank, Healer) ( idiot )")
+    if motivation == None or motivation.strip() == "":
+        raise HTTPError(400, "Motivitaion field is empty or missing.")
+
+    db.execute(f"INSERT INTO applications(name, role, motivation) VALUES ({name}, {preferred_role}, {motivation})")
 
 @app.route("/<type:re:styles|images>/<filename>")
 def server_static(type, filename):

views/join.html
@@ -2,10 +2,19 @@
 
 {% block head %}
 	<link rel="stylesheet" href="/styles/join.css">
+	<script type="module">
+		var dropdown = document.getElementById("preferredRole");
+		document.getElementById("applicationForm").addEventListener("submit", (event) => {
+			if ( dropdown.value === "invalid" ) {
+				alert("Pick a preferred role.");
+				event.preventDefault()
+			}
+		});
+	</script>
 {% endblock %}
 
 {% block content %}
-	<form method="POST" class="signup">
+	<form method="POST" class="signup" id="applicationForm">
 		<div class="signup__box">
 			<label class="signup__label" for="name">Name</label>
 			<p>
@@ -16,7 +25,7 @@
 				If your application is accepted, we will accept your in-game guild application.
 				Please make sure you have supmitted such an application in the game's UI.
 			</p>
-			<input class="signup__input" type="text" id="name" required placeholder="PoopenFarten43">
+			<input class="signup__input" type="text" id="name" name="name" required placeholder="PoopenFarten43" minlength="2" maxlength="12">
 		</div>
 		<div class="signup__box">
 			<label class="signup__label" for="preferredRole">Preferred role</label>
@@ -25,8 +34,8 @@
 				As such it's important for us to know which role you like to play as.
 				Please feel free to elaborate in text box below.
 			</p>
-			<select required class="signup__input" id="preferredRole">
-				<option selected disabled>Choose a preferred role</option>
+			<select required class="signup__input" id="preferredRole" name="preferredRole">
+				<option value="invalid" selected disabled>Choose a preferred role</option>
 				<option value="dps">DPS</option>
 				<option value="tank">Tank</option>
 				<option value="healer">Healer</option>
@@ -39,7 +48,7 @@
 				Keep in mind that we get a lot of applications for our guild;
 				a simple "i like vidya" won't get you through the golden gates!
 			</p>
-			<textarea required class="signup__input" id="motivation" rows="20" placeholder="I would like to join because..."></textarea>
+			<textarea required class="signup__input" id="motivation" name="motivation" rows="20" placeholder="I would like to join because..."></textarea>
 		</div>
 		<div>
 			<button class="signup__submit" type="submit">Submit</button>

commit b8825bf532dcbca86d07cfa7b57523051afd6a24

Save applications in database
A broken statement was introduced in 2bf130581b763819672551c138cc70119005ef93.
This patch properly initializes the database and prevents SQL injection
attacks.

app.py
@@ -18,8 +18,22 @@ AUTH_BASE_URL = 'https://oauth.battle.net/authorize'
 TOKEN_URL = "https://oauth.battle.net/token"
 client = WebApplicationClient(CLIENT_ID)
 
+DB_PATH = "thisisadatabasethatcontainsdata.db"
+
+connection = sqlite3.connect(DB_PATH)
+cursor = connection.cursor()
+cursor.executescript("""
+    CREATE TABLE IF NOT EXISTS applications (
+        username VARCHAR(12) NOT NULL,
+        preferredRole VARCHAR(6) NOT NULL,
+        motivation TEXT NOT NULL
+    );
+""")
+cursor.close()
+connection.close()
+
 app = Bottle()
-plugin = sqlite.Plugin(dbfile="thisisadatabasethatcontainsdata.db")
+plugin = sqlite.Plugin(dbfile=DB_PATH)
 app.install(plugin)
 
 @app.route("/")
@@ -52,7 +66,7 @@ def join_form():
     return template("join_form")
 
 @app.route("/join_form.html", method="POST")
-def join_submission(db):
+def join_submission(db: sqlite3.Connection):
     name = request.forms.get("name")
     preferred_role = request.forms.get("preferredRole")
     motivation = request.forms.get("motivation")
@@ -66,7 +80,7 @@ def join_submission(db):
     if motivation == None or motivation.strip() == "":
         raise HTTPError(400, "Motivitaion field is empty or missing.")
 
-    db.execute(f"INSERT INTO applications(name, role, motivation) VALUES ({name}, {preferred_role}, {motivation})")
+    db.execute(f"INSERT INTO applications(username, preferredRole, motivation) VALUES (?, ?, ?)", (name, preferred_role, motivation))
 
     return template("join_success")
 